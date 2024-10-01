72e2de90f5760419e3540e9be574a28dded6fa5d
Sıla Bozkurt <silabzkrtt@hotmail.com>
2024-10-01 23:06:08 +0300

    New lab 2 import
    new assignment

---
+++ Lab02_Q1.java
@@ -0,0 +1,26 @@
+// import Scanner
+import java.util.Scanner;
+
+public class Lab02_Q1 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner (System.in);
+        System.out.print("Enter volume of sphere: ");
+        
+
+        // define and input volume 
+        double V = in.nextDouble();
+        System.out.print(V);
+
+        // find and print radius
+        double r = Math.pow (((3 * V) / (4 * Math.PI)), 1 / 3 );
+        System.out.println("The radius of the sphere is: " +  r);
+
+        // find and print surface area
+        double A = 4 * Math.PI * (int)Math.sqrt(r);
+        System.out.println("The surface area of the sphere is: " + A);
+
+
+
+
+    }
+}
